795da05692db47af8fb41d95604944090a2971f2
MaximilianoPais <113918027+MaximilianoPais@users.noreply.github.com>
2025-09-20 16:00:00 -0300

    Add files via upload

---
+++ concesionaria.sql
@@ -0,0 +1,112 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 20-09-2025 a las 20:58:25
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `concesionaria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `motos`
+--
+
+CREATE TABLE `motos` (
+  `id_moto` int(11) NOT NULL,
+  `modelo` varchar(50) NOT NULL,
+  `precio` decimal(10,2) NOT NULL,
+  `id_tipo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `motos`
+--
+
+INSERT INTO `motos` (`id_moto`, `modelo`, `precio`, `id_tipo`) VALUES
+(1, 'Honda XR150', 3200.00, 1),
+(2, 'Yamaha R3', 5500.00, 2),
+(3, 'Zanella Styler 150', 2100.00, 3);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_motos`
+--
+
+CREATE TABLE `tipo_motos` (
+  `id_tipo` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tipo_motos`
+--
+
+INSERT INTO `tipo_motos` (`id_tipo`, `nombre`) VALUES
+(1, 'Enduro'),
+(2, 'Deportiva'),
+(3, 'Scooter');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `motos`
+--
+ALTER TABLE `motos`
+  ADD PRIMARY KEY (`id_moto`),
+  ADD KEY `id_tipo` (`id_tipo`);
+
+--
+-- Indices de la tabla `tipo_motos`
+--
+ALTER TABLE `tipo_motos`
+  ADD PRIMARY KEY (`id_tipo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `motos`
+--
+ALTER TABLE `motos`
+  MODIFY `id_moto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_motos`
+--
+ALTER TABLE `tipo_motos`
+  MODIFY `id_tipo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `motos`
+--
+ALTER TABLE `motos`
+  ADD CONSTRAINT `motos_ibfk_1` FOREIGN KEY (`id_tipo`) REFERENCES `tipo_motos` (`id_tipo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
