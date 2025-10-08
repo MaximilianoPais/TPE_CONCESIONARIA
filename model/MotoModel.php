46db7d7b259619f1d237456956def2346249fb98
MaximilianoPais <113918027+MaximilianoPais@users.noreply.github.com>
2025-10-08 20:17:53 -0300

    Update MotoModel.php

--- model/MotoModel.php
+++ model/MotoModel.php
@@ -1,32 +1,40 @@
 <?php
-require_once './model/MotoModel.php';
-require_once './view/MotoView.php';
+require_once './model/db.php';
 
-class MotoController {
-    private $model;
-    private $view;
+class MotoModel {
+    private $db;
 
     public function __construct() {
-        $this->model = new MotoModel();
-        $this->view = new MotoView();
+        $this->db = getConnection();
     }
 
-    // (A) Listado de motos
-    public function listarMotos() {
-        $motos = $this->model->getMotos();
-        $this->view->mostrarMotos($motos);
+    // (A) Listado de todas las motos
+    public function getMotos() {
+        $query = $this->db->prepare("SELECT m.id_moto, m.modelo, m.precio, t.nombre AS tipo
+                                     FROM motos m
+                                     JOIN tipo_motos t ON m.id_tipo = t.id_tipo");
+        $query->execute();
+        return $query->fetchAll(PDO::FETCH_OBJ);
     }
 
-    // (A) Detalle de moto
-    public function mostrarDetalle($id) {
-        $moto = $this->model->getMotoById($id);
-        $this->view->mostrarDetalleMoto($moto);
+    // (B) Detalle de una moto
+    public function getMotoById($id) {
+        $query = $this->db->prepare("SELECT m.id_moto, m.modelo, m.precio, t.nombre AS tipo
+                                     FROM motos m
+                                     JOIN tipo_motos t ON m.id_tipo = t.id_tipo
+                                     WHERE m.id_moto = ?");
+        $query->execute([$id]);
+        return $query->fetch(PDO::FETCH_OBJ);
     }
 
-    // (B) Motos por categoría
-    public function listarPorCategoria($id_tipo) {
-        $motos = $this->model->getMotosByCategoria($id_tipo);
-        $this->view->mostrarMotos($motos);
+    // (C) Listado de motos por categoría
+    public function getMotosByCategoria($id_tipo) {
+        $query = $this->db->prepare("SELECT id_moto, modelo, precio 
+                                     FROM motos
+                                     WHERE id_tipo = ?");
+        $query->execute([$id_tipo]);
+        return $query->fetchAll(PDO::FETCH_OBJ);
     }
 }
+
 
